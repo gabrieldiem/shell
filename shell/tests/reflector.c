9912a1f607e7b027cf290cd13895e34307e7a2d6
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2024-04-23 20:24:34 -0300

    refactor: improve tests/reflector.c util program

--- shell/tests/reflector.c
+++ shell/tests/reflector.c
@@ -9,11 +9,20 @@
 #include <unistd.h>
 #include <string.h>
 
-int
+#define SYMLINK_BUFSIZE 1024
+
+static int
 inspect_fds(int out_fd)
 {
 	struct dirent *entry;
+
 	DIR *dir = opendir("/proc/self/fd");
+
+	if (!dir) {
+		perror("opendir failed");
+		exit(EXIT_FAILURE);
+	}
+
 	int dir_fd = dirfd(dir);
 
 	while ((entry = readdir(dir))) {
@@ -26,9 +35,9 @@ inspect_fds(int out_fd)
 			continue;
 		}
 
-		char target_link_buf[100];
-		ssize_t len =
-		        readlinkat(dir_fd, entry->d_name, target_link_buf, 100);
+		char target_link_buf[SYMLINK_BUFSIZE];
+		ssize_t len = readlinkat(
+		        dir_fd, entry->d_name, target_link_buf, SYMLINK_BUFSIZE);
 		target_link_buf[len] = 0;
 
 		dprintf(out_fd, "%s\t%s\n", entry->d_name, target_link_buf);
@@ -39,6 +48,23 @@ inspect_fds(int out_fd)
 	return 0;
 }
 
+static void
+log_process_file_descriptors(char *output_filename)
+{
+	int out_fd;
+
+	out_fd = open(output_filename, O_CREAT | O_TRUNC | O_WRONLY, 0644);
+
+	if (out_fd < 0) {
+		perror("open failed");
+		exit(EXIT_FAILURE);
+	}
+
+	inspect_fds(out_fd);
+
+	close(out_fd);
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -46,16 +72,7 @@ main(int argc, char *argv[])
 		printf("Usage: %s <target_file>\n", argv[0]);
 	}
 
-	int out_fd = open(argv[1], O_CREAT | O_TRUNC | O_WRONLY, 0644);
-
-	if (out_fd < 0) {
-		perror("Error en open");
-		return -1;
-	}
-
-	inspect_fds(out_fd);
-
-	close(out_fd);
+	log_process_file_descriptors(argv[1]);
 
 	return 0;
 }
