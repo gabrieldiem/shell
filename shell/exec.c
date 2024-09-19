39dfd4a9a231e98cad64307cdef15a59a35bb99f
gabrieldiem <gabriel.diem5@gmail.com>
2024-09-19 17:48:05 -0300

    feat: Added stream 2 to stream 1 redirection

--- shell/exec.c
+++ shell/exec.c
@@ -1,5 +1,8 @@
 #include "exec.h"
 
+static const char STR_COMBINE_STREAM_2_INTO_STREAM_1[] = "&1";
+static const int LEN_STR_COMBINE_STREAM_2_INTO_STREAM_1 = 2;
+
 // sets "key" with the key part of "arg"
 // and null-terminates it
 //
@@ -70,8 +73,29 @@ open_redir_fd(char *file, int flags)
 }
 
 void
-load_arguments(struct execcmd *exec_cmd)
+run_exec(struct execcmd *exec_cmd)
 {
+	char *execvp_buff[MAXARGS + 1] = { NULL };
+
+	for (int i = 0; i < exec_cmd->argc; i++) {
+		execvp_buff[i] = exec_cmd->argv[i];
+	}
+
+	execvp(exec_cmd->argv[0], execvp_buff);
+}
+
+bool
+should_combine_stream_2_into_stream_1(struct execcmd *exec_cmd)
+{
+	return strncmp(exec_cmd->err_file,
+	               STR_COMBINE_STREAM_2_INTO_STREAM_1,
+	               LEN_STR_COMBINE_STREAM_2_INTO_STREAM_1) == 0;
+}
+
+void
+combine_stream_2_into_stream_1()
+{
+	dup2(STDOUT_FILENO, STDERR_FILENO);
 }
 
 // executes a command - does not return
@@ -84,22 +108,17 @@ void
 exec_cmd(struct cmd *cmd)
 {
 	// To be used in the different cases
-	struct execcmd *e;
+	struct execcmd *exec_cmd;
 	struct backcmd *b;
-	struct execcmd *r;
+	struct execcmd *redir_cmd;
 	struct pipecmd *p;
 
 	switch (cmd->type) {
 	case EXEC:
-		e = (struct execcmd *) cmd;
-		char *execvp_buff[MAXARGS + 1] = { NULL };
-		for (int i = 0; i < e->argc; i++) {
-			execvp_buff[i] = e->argv[i];
-		}
+		exec_cmd = (struct execcmd *) cmd;
+		run_exec(exec_cmd);
 
-		execvp(e->argv[0], execvp_buff);
-
-		_exit(-1);
+		_exit(EXIT_FAILURE);
 		break;
 
 	case BACK: {
@@ -107,7 +126,7 @@ exec_cmd(struct cmd *cmd)
 		//
 		// Your code here
 		printf("Background process are not yet implemented\n");
-		_exit(-1);
+		_exit(EXIT_FAILURE);
 		break;
 	}
 
@@ -118,9 +137,15 @@ exec_cmd(struct cmd *cmd)
 		// verify if file name's length (in the execcmd struct)
 		// is greater than zero
 		//
-		// Your code here
-		printf("Redirections are not yet implemented\n");
-		_exit(-1);
+		redir_cmd = (struct execcmd *) cmd;
+
+		if (should_combine_stream_2_into_stream_1(redir_cmd)) {
+			combine_stream_2_into_stream_1();
+		}
+
+		run_exec(redir_cmd);
+
+		_exit(EXIT_FAILURE);
 		break;
 	}
 
