e565d3e887dffb4ebdabd81e8018b850ee5438e6
gabrieldiem <gabriel.diem5@gmail.com>
2024-09-26 22:29:14 -0300

    feat: improved print_status functions. Added print status from PID only for signal handler

--- shell/printstatus.c
+++ shell/printstatus.c
@@ -37,6 +37,39 @@ print_status_info(struct cmd *cmd)
 #endif
 }
 
+// prints information of process' status from a PID
+void
+print_status_info_from_pid(int pid, int _status)
+{
+	const char *action;
+
+	if (WIFEXITED(_status)) {
+		action = "exited";
+		_status = WEXITSTATUS(_status);
+	} else if (WIFSIGNALED(_status)) {
+		action = "killed";
+		_status = -WTERMSIG(_status);
+	} else if (WTERMSIG(_status)) {
+		action = "stopped";
+		_status = -WSTOPSIG(_status);
+	} else {
+		action = "exited";
+	}
+
+#ifndef SHELL_NO_INTERACTIVE
+	if (isatty(1)) {
+		fprintf(stdout,
+		        "%s	Program with PID [%d] %s, status: "
+		        "%d %s\n",
+		        COLOR_BLUE,
+		        pid,
+		        action,
+		        _status,
+		        COLOR_RESET);
+	}
+#endif
+}
+
 // prints info when a background process is spawned
 void
 print_back_info(struct cmd *back)
--- shell/printstatus.h
+++ shell/printstatus.h
@@ -10,4 +10,6 @@ void print_status_info(struct cmd *cmd);
 
 void print_back_info(struct cmd *back);
 
+void print_status_info_from_pid(int pid, int status);
+
 #endif  // PRINTSTATUS_H
